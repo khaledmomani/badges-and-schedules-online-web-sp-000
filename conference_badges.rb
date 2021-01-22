91d88b590082815cc7e9f447f1b65c1e8ee24f4c
Khaled S Momani <khaledmomani4@gmail.com>
2021-01-22 04:04:21 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -17,7 +17,7 @@ def assign_rooms(names)
 end 
 
 def printer(attendee)
-  attendee.map do |name|
+  attendee.each_with_index.map do |name|
   puts "Hello, #{name}! You'll be assigned to room #{index+1}!"
 end 
 end
